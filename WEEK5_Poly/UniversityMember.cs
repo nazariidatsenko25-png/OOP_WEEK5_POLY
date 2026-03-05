e2452f874e09516946098331c075bebb0bcfe594
nazariidatsenko25-png <nazarii.datsenko.25@kse.org.ua>
2026-03-05 22:24:55 +0200

    comit

---
+++ WEEK5_Poly/UniversityMember.cs
@@ -0,0 +1,35 @@
+﻿namespace WEEK5_Poly;
+
+public class UniversityMember
+{
+    private string name;
+    public string Name { get => name;
+        private set
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException("name cannot be empty");
+            name = value;
+        }
+    }
+    private string memberID;
+    public string MemberID { get => memberID; private set => memberID = value; }
+
+    protected List<string> Actionlog = new List<string>();
+    public UniversityMember(string name, string memberID)
+    {
+        Name = name;
+        MemberID = memberID;
+    }
+    public virtual void PerformDuties()
+    {
+        if (Actionlog.Count >= 5)
+        {
+            throw new Exception("daily limit was reached");
+        }
+    }
+    public int GetActionlogCount()
+    {
+        return Actionlog.Count;
+    }
+}
+
+
